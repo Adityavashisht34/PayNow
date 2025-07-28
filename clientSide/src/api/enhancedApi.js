abbb8a16ec1554dd3773dbd1ea1af5a7bb8013e6
Adityavashisht34 <adityavashisht34@gmail.com>
2025-07-28 11:37:06 +0530

    API fixed

--- clientSide/src/api/enhancedApi.js
+++ clientSide/src/api/enhancedApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = import.meta.env.VITE_APP_URL;
-const API_URL = "http://localhost:8080"
+const API_URL = import.meta.env.VITE_APP_URL;
+
 
 
 export const userApi = {
--- clientSide/src/api/simpleApi.js
+++ clientSide/src/api/simpleApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-//const API_URL = import.meta.env.VITE_APP_URL;
-const API_URL = "http://localhost:8080"
+const API_URL = import.meta.env.VITE_APP_URL;
+
 
 
 export const userApi = {
